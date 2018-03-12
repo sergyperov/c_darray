8949c107235b4378eb3580a04291aa9986826eb9
Sergy Perov <sergyperov@gmail.com>
2018-03-12 11:57:50 +0300

    main.c changed

--- main.c
+++ main.c
@@ -1,28 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "char_darray.h"
-
-void char_darray_print(darray* darr) {
-    int darr_size = darray_size(darr);
-    for (int i = 0; i < darr_size; i++) {
-        printf("%c\n",char_darray_get_elem_by_index(darr, i));
-    }
-}
+#include "darray_wrappers.h"
 
 int main() {
-    darray* darr = darray_new();
-    char_darray_push_back(darr, 'a');
-    char_darray_push_back(darr, 'b');
-    char_darray_push_back(darr, 'c');
-    char_darray_push_back(darr, 'd');
-    char_darray_push_front(darr, 'e');
-    char_darray_print(darr);
-    printf("\n\n");
-    char_darray_insert_at_index(darr, 'k', 4);
-    
-    char_darray_print(darr);
-    
-    
-
+    darray* darr = darray_new(sizeof(int));
+    char x = 'h';
+    darray_push_front(darr, &x);
+    x = 't';
+    printf("%c ", char_darray_get_elem_by_index(darr, 0));
 }
